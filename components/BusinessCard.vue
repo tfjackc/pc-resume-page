605fc82c7dcf2815044fe1580f7e64777a718230
tfjackc <colpitt.jack@gmail.com>
2023-12-15 12:04:43 -0800

    qr code added

---
+++ components/BusinessCard.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<v-container>
+  <v-row>
+    <v-col class="top-div d-flex justify-center">
+      <h1>QR Code</h1>
+    </v-col>
+    <v-col class="top-div d-flex justify-center">
+        <div class="ma-4">
+          <v-img
+              class="bg-white"
+              width="300"
+              :aspect-ratio="1"
+              src="assets/frame.png"
+              cover
+          ></v-img>
+        </div>
+
+    </v-col>
+  </v-row>
+</v-container>
+</template>
+s
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,20 +6,17 @@
     </v-window-item>
     <v-window-item
         :value="2">
-      <v-col>
 
-      </v-col>
     </v-window-item>
     <v-window-item
         :value="3">
-      <v-col>
-
-      </v-col>
+        <BusinessCard />
     </v-window-item>
   </v-window>
 </template>
 
 <script setup lang="ts">
+import BusinessCard from "~/components/BusinessCard.vue";
 import { useDataStore } from "~/store/data_store";
 const dataStore = useDataStore();
 const { tab } = storeToRefs(dataStore);
