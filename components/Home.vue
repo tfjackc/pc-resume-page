c31aecbbe7007418b9a24cb48719c8ae793ad328
tfjackc <pnwest.geo@gmail.com>
2023-12-19 13:08:35 -0800

    good spot

--- components/Home.vue
+++ components/Home.vue
@@ -2,17 +2,17 @@
   <v-container>
     <v-card elevation="10">
     <v-row>
-      <v-col class="d-flex justify-center">
-        <div class="ma-4">
+      <v-col class="d-flex justify-center" cols="12">
+        <div>
           <NuxtImg
               src="/paigevoegeli_logo_blacktext.png"
               sizes="100vw sm:80vw md:600px lg:700px xl:800px"
           />
         </div>
       </v-col>
-      <v-col class="about-div ma-6">
+      <v-col class="about-div ma-5 pa-5 mb-10 d-flex flex-column align-center justify-center" cols="12">
         <h1>Tulsa, Oklahoma, United States</h1>
-        <h3><i>10+ Years of Industry Experience</i></h3>
+        <h3><i>11+ Years of Industry Experience</i></h3>
         <p>Graduated in the fall of 2013 from The University of Tulsa with a bachelor’s degree in Petroleum Engineering. Paige is intimately familiar with the complexities of the oil and gas industry, being raised in a family that has owned, drilled, completed and operated wells the industry for three generations.</p>
       </v-col>
     </v-row>
