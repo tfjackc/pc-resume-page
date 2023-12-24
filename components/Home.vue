77d874ae3e941140f07fe9c0927438abcb4d9559
tfjackc <colpitt.jack@gmail.com>
2023-12-23 21:39:25 -0800

    fixed text a bit

--- components/Home.vue
+++ components/Home.vue
@@ -20,7 +20,7 @@
             </NuxtImg>
         </div>
       </v-col>
-      <v-col class="about-div ma-5 pa-5 mb-10 d-flex flex-column align-center justify-center" cols="12">
+      <v-col class="about-div pa-10 mb-10 d-flex flex-column align-center justify-center" cols="12">
         <h1>Tulsa, Oklahoma, United States</h1>
         <h3><i>11+ Years of Industry Experience</i></h3>
         <p>Graduated in the fall of 2013 from The University of Tulsa with a bachelor’s degree in Petroleum Engineering. Paige is intimately familiar with the complexities of the oil and gas industry, being raised in a family that has owned, drilled, completed and operated wells the industry for three generations.</p>
